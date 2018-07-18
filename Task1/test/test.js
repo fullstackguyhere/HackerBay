9bb1b22e7355a20ebf6ed12acf8747f4cd28d046
fullstackguyhere <fullstackguyhere@gmail.com>
2018-07-19 00:57:17 +0530

    travis ci final changes

--- Task1/test/test.js
+++ Task1/test/test.js
@@ -1,40 +1,41 @@
 var should = require('chai').should(),
     supertest = require('supertest'),
-    api = supertest('http://localhost:3000');
-
+    api = supertest('http://localhost:3000'),
+    app =require('../app');
+    
 describe('Testing Basic Api',function(){
     it('Testing first get', function(done){
-        api.get('/').expect(200).expect('Content-Type', /json/).expect({status: 'success'},done);
+        supertest(app).get('/').expect(200).expect('Content-Type', /json/).expect({status: 'success'},done);
     });
 
     var postObj = {data: 'heheheh'};
     
     it('Testing second get-1', function(done){
-        api.get('/data').expect(400,done);
+        supertest(app).get('/data').expect(400,done);
     });
     it('Testing Post--Negative-1',function(done)
     {
-       api.post('/data').send().set('Accept', 'application/json').expect(400,done);
+        supertest(app).post('/data').send().set('Accept', 'application/json').expect(400,done);
     });
 
     it('Testing Post--Negative-2',function(done)
     {
-       api.post('/data').send({data:1}).set('Accept', 'application/json').expect(400,done);
+        supertest(app).post('/data').send({data:1}).set('Accept', 'application/json').expect(400,done);
     });
 
     it('Testing Post--Negative-3',function(done)
     {
-       api.post('/data').send({data:{"asd":"asdasd"}}).set('Accept', 'application/json').expect(400,done);
+        supertest(app).post('/data').send({data:{"asd":"asdasd"}}).set('Accept', 'application/json').expect(400,done);
     });
 
     it('Testing Post',function(done)
     {
-       api.post('/data').send(postObj).set('Accept', 'application/json').expect(200).expect(postObj,done);
+        supertest(app).post('/data').send(postObj).set('Accept', 'application/json').expect(200).expect(postObj,done);
     });
 
     it('Testing Post--Negative-3-4',function(done)
     {
-       api.post('/data').send({data:{"asd":"asdasd"}}).set('Accept', 'application/json').expect(400,done);
+        supertest(app).post('/data').send({data:{"asd":"asdasd"}}).set('Accept', 'application/json').expect(400,done);
     });
 
     
